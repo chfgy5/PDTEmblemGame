43389240738864701e7afc9a798e1755c99a3a42
Caleb Fagan <Caleb.Fagan@vu.com>
2018-06-22 11:27:43 -0500

    Added Player images and bench to game view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { BoardComponent } from './board/board.component';
+import { GameViewComponent } from './game-view/game-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'board', component: BoardComponent}
+  { path: 'board', component: BoardComponent },
+  { path: 'gameview', component: GameViewComponent }
 ];
 
 @NgModule({
